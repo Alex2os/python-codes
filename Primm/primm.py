b79d7a22e6bb335e347619528af4b44f1b436231
Alex <144067034+AlexCats1@users.noreply.github.com>
2026-05-10 21:26:37 -0600

    primm now has vertices in the original map and in the mask

--- Primm/primm.py
+++ Primm/primm.py
@@ -2,16 +2,17 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
-img = cv2.imread("mapa3.PNG") # cargamos nuestro mapa en formato png. en este caso puede ser cualquiera de los 4 mapas que se nos proporcionaron
+img = cv2.imread("mapa3.PNG")
 img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+vertices = np.load("verticeMapa3.npy")
 
 R = img_rgb[:, :, 0]
 G = img_rgb[:, :, 1]
 B = img_rgb[:, :, 2]
 
 # mascara para las calles. en este caso buscamos que las calles se noten con el color blanco en rgb (255, 255, 255)
-tolerancia_calles = 4 # tenemos una tolerancia de 4, poniendo mas o menos que esto da algo de ruido en la mascara final
-
+tolerancia_calles = 4
 
 mascara_calles = (
     (R >= 255 - tolerancia_calles) &
@@ -19,7 +20,7 @@ mascara_calles = (
     (B >= 255 - tolerancia_calles)
 )
 
-# realizamos una lista con los colores exactos, extraidos de los mapas proporcionados. de esta forma obtenemos las mascaras lo mas limpias posibles para poder trabajar posteriormente con ellas
+# realizamos una lista con los colores exactos, extraidos de los mapas proporcionados.
 colors = [
     ("#858d93", 0),
     ("#c5c9d9", 0),
@@ -51,32 +52,30 @@ for hex_color, tolerance in colors:
 
     mascara_letras_calle = mascara_letras_calle | current_mask
 
-# combinar las mascaras que realizamos, usando la mascara de la calle y la de los demas colores de las letras y flechas del mapa
+# combinar las mascaras que realizamos
 mask = mascara_calles | mascara_letras_calle
 mask = mask.astype(np.uint8) * 255
 
-# -----------------------------
-# 4) Clean and connect
-# -----------------------------
+# limpiar y conectar
 kernel = np.ones((3, 3), np.uint8)
 
 mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel, iterations=2)
 mask = cv2.dilate(mask, kernel, iterations=1)
 mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel, iterations=1)
 
-# -----------------------------
-# 5) Show and save
-# -----------------------------
+# mostrar mapa original con vertices y mascara con vertices
 plt.figure(figsize=(12, 6))
 
 plt.subplot(1, 2, 1)
 plt.imshow(img_rgb)
-plt.title("Original Map")
+plt.scatter(vertices[:, 1], vertices[:, 0], s=35)
+plt.title("Original Map with Vertices")
 plt.axis("off")
 
 plt.subplot(1, 2, 2)
 plt.imshow(mask, cmap="gray")
-plt.title("Road Mask")
+plt.scatter(vertices[:, 1], vertices[:, 0], s=35)
+plt.title("Road Mask with Vertices")
 plt.axis("off")
 
 plt.show()
